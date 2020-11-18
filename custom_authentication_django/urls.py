63da4ede16a2e64167da800a82c13297dc4e0f27
Kallebe Felipe <kallebefelipe@gmail.com>
2020-11-17 23:26:37 -0300

    Adicionando urls de enpoints

--- custom_authentication_django/urls.py
+++ custom_authentication_django/urls.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+
+from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
         'signin/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
+        views.SigninView.as_view(),
+        name='signin'
+    ),
+    path(
+        'signup/',
+        views.SignupView.as_view(),
+        name='signup'
+    ),
+    path(
+        'me/',
+        views.MeView.as_view(),
+        name='me'
     )
 ]
